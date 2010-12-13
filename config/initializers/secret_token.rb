d1c416d7aa3080d1f40d6d171d3d2689b6eccb94
hideto.bj@gmail.com <Hideto@pek-fwm-dhcp-c170.dev.fwmrm.net>
2010-12-13 16:39:21 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WendaIt::Application.config.secret_token = '0ca2b35775cbd8a001baae8b0302dc645111d0959ed9a381d5a63820ceb400e2a8f68d19c846ec4999291dc0ec3bb1d1822bcb839fb9ccb52e6ff585d0c3cc54'
