a33e2cd5fbf0b7bd2ad794abe545dc3fab491c99
dash2011 <dash.2011.dash@gmail.com>
2011-01-13 17:38:47 +0800

    Revert "login with sina/douba"
    This reverts commit 69fd64bd46362382f565ba30ca8b811a56c2d8a8.

--- config/environment.rb
+++ config/environment.rb
@@ -19,8 +19,8 @@ DEFAULT_TITLE = "wenda.it - 做国内最好的IT专业知识问答网站"
 DEFAULT_META = "wenda.it,Java,Spring,Hibernate,Struts,Eclipse,AJAX,Ruby,Ruby on rails,Python,Django,jQuery,json,JavaScript,Android,iphone,mac os,objective-c,cocoa,html,html5,css,Flex,Flash,C#,.net,C,C++,Erlang,PHP,ubuntu,Linux,Agile,Scrum,XP,TDD,SQL,MySQL,Oracle,NOSQL,mongodb,svn,git"
 DEFAULT_KEYWORDS = "wenda.it Java AJAX Ruby Ruby on rails Linux Agile Scrum MySQL NOSQL mongodb"
 
-# Sina/Douban API
-SINA_API_KEY = ""
-SINA_API_KEY_SECRET = ""
-DOUBAN_API_KEY = ""
-DOUBAN_API_KEY_SECRET = ""
+# Douban/Sina API
+DOUBAN_API_KEY = "0ba792a10764637022f6b7e3162d2a50"
+DOUBAN_API_KEY_SECRET = "05d3281b7f194008"
+SINA_API_KEY = "3246097489"
+SINA_API_KEY_SECRET = "61b8d9bf1a747f18b65f357ec64769f3"
